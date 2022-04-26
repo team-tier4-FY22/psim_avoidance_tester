42bd51d22e93e96307e39922638b58d77029b541
Mamoru Sobue <mamoru.sobue@rtier4.jp>
2022-04-26 17:40:30 +0900

    setup python install

--- scripts/dummy_publisher.py
+++ scripts/dummy_publisher.py
@@ -6,6 +6,7 @@ import launch
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
 import rclpy.node
+from dummy_perception_publisher.msg import Object
 
 class DummyPublisher(rclpy.node.Node):
     def __init__(self):
@@ -16,15 +17,15 @@ class DummyPublisher(rclpy.node.Node):
         yaml_info = yaml.safe_load(yaml_path_fp)
         pedestrians = yaml_info["pedestrians"]
         self.pedestrians = dict() # map ["name"] => [x, y, z]
-        n_pedestrians = len(pedestrians)
-        for i in range(n_pedestrians):
+        for i in range(len(pedestrians)):
             pedestrian_name = list(pedestrians[i].keys())[0]
             position = pedestrians[i][pedestrian_name]["position"]
             self.pedestrians[pedestrian_name] = []
             self.pedestrians[pedestrian_name].append(position['x'])
             self.pedestrians[pedestrian_name].append(position['y'])
             self.pedestrians[pedestrian_name].append(position['z'])
-            
+
+        
 def main(args=None):
     rclpy.init(args=args)
     p = DummyPublisher()
